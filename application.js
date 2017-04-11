3248db8873fe8fc97f517adcf401e48f2c5d4658
ventaquil <konrad.golawski@gmail.com>
2017-04-11 19:35:18 +0200

    Log data in main application file

--- application.js
+++ application.js
@@ -4,6 +4,7 @@ const database = require('./application/helpers/database');
 const directory = require('./application/helpers/directory');
 const express = require('express');
 const file_upload = require('express-fileupload');
+const logger =  require('./application/helpers/logger');
 const process = require('process');
 const routes = require('./application/routes');
 
@@ -18,13 +19,13 @@ app.use(file_upload()); // Add file upload module
 app.use(express.static(directory.public())); // Set static folder
 
 database.connect(function (err, db) {
-    console.log('MongoDB is running (port ' + database.port + ', database ' + database.database + ')');
+    logger.log('MongoDB is running (port ' + database.port + ', database ' + database.database + ')', true);
 
     db.close();
 });
 
 app.listen(port, function () { // Listen on specified port
-    console.log('Listening on port ' + port);
+    logger.log('Listening on port ' + port, true);
 });
 
 routes.register(app); // Register routes
