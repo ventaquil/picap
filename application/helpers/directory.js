69ab743fb4af2db31791e265cbeadc443b3bfb3f
ventaquil <konrad.golawski@gmail.com>
2017-04-19 20:49:53 +0200

    Add assets directories in directory application module

--- application/helpers/directory.js
+++ application/helpers/directory.js
@@ -3,6 +3,12 @@
 const path = require('path');
 
 module.exports = {
+    'assets': function () {
+        return path.join(__dirname, '..', '..', 'assets');
+    },
+    'javascript': function () {
+        return path.join(this.assets(), 'javascript');
+    },
     'logs': function () {
         return path.join(this.storage(), 'logs');
     },
@@ -14,6 +20,9 @@ module.exports = {
     },
     'public': function () {
         return path.join(__dirname, '..', '..', 'public');
+    },
+    'sass': function () {
+        return path.join(this.assets(), 'sass');
     },
     'trash': function () {
         return path.join(this.storage(), 'trash');
